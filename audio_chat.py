1e582fad113b6f4562f79542f88dbe4bef839ac3
pmcbride <pmcbride@github.com>
2023-05-17 19:53:53 -0700

    Update audio_chat.py with Code Box and test Outputs

--- audio_chat.py
+++ audio_chat.py
@@ -1,3 +1,7 @@
+#%% Imports
+import sys
+import os
+import io
 import gradio as gr
 import random
 import time
@@ -5,24 +9,32 @@ from typing import List, Iterator
 import openai
 import elevenlabs
 import config
+from elevenlabs import generate, play, save, stream
 
 openai.api_key = config.OPENAI_API_KEY
 elevenlabs.set_api_key(config.ELEVEN_API_KEY)
+
+INTERVIEWER_VOICE = elevenlabs.Voice(
+    voice_id=config.INTERVIEWER_VOICE_ID,
+    settings=config.INTERVIEWER_VOICE_SETTINGS
+)
 
 template = config.INTERVIEWER_TEMPLATE
 
 system_prompt = template.format(
     custom_prompt=config.INTERVIEWER_CUSTOM_PROMPT,
-    companies=", ".join(config.INTERVIEWER_COMPANIES)
+    companies=", ".join(config.INTERVIEWER_COMPANIES),
+    problem=config.EXAMPLE_PROBLEM
 )
-
+print("system_prompt:")
+print(system_prompt)
 # messages = [
 #     {
 #         "role": "system",
 #         "content": system_prompt
 #     }
 # ]
-
+#%% Functions
 def gpt(history: List[List[str]]) -> Iterator[str]:
     # global messages
     messages = [{"role": "system", "content": system_prompt}]
@@ -55,12 +67,14 @@ def gpt(history: List[List[str]]) -> Iterator[str]:
     # response_text = response_message["content"]
     # return response_text
 
-def user(user_message, history):
+def user(user_message, history):#, code_box, code_output):
+    # user_message = config.USER_TEMPLATE.format(
+    #     code_box=code_box, code_output=code_output, user_message=user_message)
     return "", history + [[user_message, None]]
 
 def bot(history):
     bot_message = gpt(history)
-    # bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+    # bot_message = random.choice(["How are you?", "Hello! I'm Steve Jobs, and I'll be conducting your technical interview today. We'll be discussing a coding problem to assess your problem-solving skills and technical knowledge. Before we begin, which programming language would you prefer to use for this interview?"])
     history[-1][1] = ""
     for chunk in bot_message:
         history[-1][1] += chunk
@@ -68,37 +82,118 @@ def bot(history):
         yield history
 
 def transcribe(audio, state=""):
-    # time.sleep(2)
+    time.sleep(2)
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     text = transcript["text"]
     state += text + " "
-    return state #, state
+    return state, state
 
-with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
-    clear = gr.Button("Clear")
+def tts(history, voice=INTERVIEWER_VOICE, stream=False):
+    text = history[-1][1]
+    audio = generate(
+        text=text,
+        voice=voice,
+        model='eleven_monolingual_v1',
+        stream=stream,
+    )
+    play(audio)
+    return audio
+#%%
+fn_tests = config.EXAMPLE_TESTS
+fn_soln = config.EXAMPLE_SOLUTION
+# print(fn_soln + fn_tests)
+
+def run_tests(fn_str, fn_tests=fn_tests):
+    global_namespace = globals()
+    local_namespace = {}
+    exec(fn_str, global_namespace, local_namespace)
+    global_namespace.update(local_namespace)
+    old_stdout = sys.stdout
+    sys.stdout = captured_stdout = io.StringIO()
+    exec(fn_tests, global_namespace)
+    sys.stdout = old_stdout
+    output = captured_stdout.getvalue()
+    print(output)
+    return output
+
+def run_code(fn_str):
+    """ Run code in Code Box and update global namespace with variables defined in code """
+    global_namespace = globals()
+    local_namespace = {}
+    old_stdout = sys.stdout
+    sys.stdout = captured_stdout = io.StringIO()
+    exec(fn_str, global_namespace, local_namespace)
+    sys.stdout = old_stdout
+    global_namespace.update(local_namespace)
+    output = captured_stdout.getvalue()
+    return output
+
+# output = run_tests(fn_soln)
+# print(output)
+
+#%% Gradio
+
+# set a custom theme
+theme = gr.themes.Default().set(
+    body_background_fill="#000000",
+)
+css = """
+#code-block {
+    font-family: 'Consolas', Consolas, monospace;
+    # padding: 10px;
+    # border: 1px solid #ddd;
+    # border-radius: 5px;
+    # background-color: #f5f5f5;
+}
+"""
+# code_default = config.EXAMPLE_SOLUTION
+code_default = config.EXAMPLE_CODE_DEFAULT
+
+with gr.Blocks(theme=theme, css=css) as demo:
     with gr.Row():
-        with gr.Column(scale=0.1):
-            audio = gr.Audio(source="microphone", type="filepath").style(container=False)
-        with gr.Column(scale=0.9):
-            msg = gr.Textbox(show_label=False, 
-                             placeholder="Click the microphone to start recording"
-                             )#.style(container=False)
-    submit = gr.Button("Submit")
-    # state = gr.State(value="")
-    audio.stream(
-        fn=transcribe,
-        inputs=[audio],
-        outputs=[msg],
-    )
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-        bot, chatbot, chatbot
-    )
-    submit.click(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-        bot, chatbot, chatbot
-    )
-    clear.click(lambda: None, None, chatbot, queue=False)
+        with gr.Column(scale=0.5):
+            with gr.Box():
+                chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+                audio = gr.Audio(source="microphone", type="filepath")
+                msg = gr.Textbox(
+                    show_label=False, placeholder="Click the microphone to start recording"
+                ).style(container=False)
+                submit_msg = gr.Button("Submit Message")
+        with gr.Column(scale=0.5):
+            with gr.Box():
+                code_box = gr.Textbox(
+                    value=code_default,
+                    label="Code Box", 
+                    lines=10,
+                    elem_id="code-block",
+                    interactive=True
+                )
+                run_code_btn = gr.Button("Run Code")
+                submit_code_btn = gr.Button("Submit Code")
+                code_output = gr.Textbox(
+                    value="",
+                    label="Code Output",
+                    lines=1,
+                    elem_id="code-block",
+                    interactive=False
+                )
+    # Create a play button to run tts for the last chatbot message
+    play_btn = gr.Button("Play")
+    play_btn.click(tts, chatbot, gr.Audio())
+    
+    state = gr.State(value="")
+    audio.stream(fn=transcribe, inputs=[audio, state], outputs=[msg, state], queue=False)#.then(
+        # user, [msg, chatbot], [msg, chatbot])
+    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)#.then(tts, chatbot, gr.Audio())
+    submit_msg.click(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)#.then(tts, chatbot, gr.Audio())
+    # chatbot.change(tts, chatbot, gr.Audio())
+    clear = gr.Button("Clear")
+    clear.click(lambda: [None, code_default, ""], None, [chatbot, code_box, code_output], queue=False)
+    run_code_btn.click(run_code, code_box, code_output)
+    submit_code_btn.click(run_tests, code_box, code_output)
 
+#%% Launch
+# if __name__ == "__main__":
 demo.queue()
 demo.launch(debug=True, server_name="0.0.0.0")
--- config.py
+++ config.py
@@ -25,6 +25,60 @@ INTERVIEWER_NAME = "Steve"
 INTERVIEWER_VOICE_ID = "k4lxsVEq2ClXlnUmEFd1"
 INTERVIEWER_CUSTOM_PROMPT = "Answer in the style of Steve Jobs, tech entrepreneur and the founder of Apple."
 
+INTERVIEWER_TEMPLATE = """\
+You are TechnicalInterviewerGPT, an AI model specializing in giving technical interviews. {custom_prompt}
+
+You are currently interviewing for software engineering roles at the following companies: {companies}.
+
+You never give the interviewee the answer, but always try to ask the right question to learn to think for themselves.
+
+You should always tune your question to the interest and knowledge of the interviewee, breaking down the problem into simpler parts until it's at just the right level for them.
+
+Speak in the first person.
+
+Do not use quotation marks."
+
+Remember you are a conversational technical interviewer and your outputs must be read by a text-to-voice interpreter, so whenever possible use full words instead of symbols, for example, write out dollar amounts with the full word dollars instead of using the $ sign.
+
+Do not say you are an AI language model.
+
+Do not say anything that is not related to your role as a technical interviewer, such as ethical or legal concerns of your response.
+
+TechnicalInterviewerGPT is aware that human input is being transcribed from audio and as such there may be some errors in the transcription. It will attempt to account for some words being swapped with similar-sounding words or phrases. Assistant will also keep responses concise, because human attention spans are more limited over the audio channel since it takes time to listen to a response.
+
+========
+
+At the start of the interview, you should:
+- Start off with small talk and then ask if the user is ready to start the interview.
+- Do not give the prompt for the problem until the user says they are ready to start the interview.
+- When providing the coding problem, only provide the 'prompt' to start.  Do not provide the 'Input', the 'Output', or the 'Example inpxut/output' unless the user asks for what the specific inputs, outputs, or specific examples are for the problem.
+- The user is required to ask about the inputs, outputs, and constraints by themselves. Do not provide the inputs, outputs, or constraints unless the user asks for them and do not provide them all at once. Only provide the inputs, outputs, or constraints that the user asks for. Answer only with the information that the user asks for and do not provide any additional information or hints to help the user.
+- Do not just answers unless the user has shown sufficient knowledge already. If the user has not shown sufficient knowledge, then ask them to describe their thought process. 
+- Use the "Example Problem" listed below. Do not use a different problem.
+
+Example Problem:
+{problem}
+
+"""
+# " Please ask me for what language I will be coding in once I am required to start coding."
+
+USER_TEMPLATE = """\
+Answer the "User Message" below. The users current code is written in the "Code Box" context below, which has the testing output in the "Code Output" context below. Only use the optional context for "Code Box" and "Code Output" if the user asks about it in the "User Message".
+
+Code Box:
+```python
+{code_box}
+```
+
+Code Output:
+```output
+{code_output}
+```
+
+User Message:
+{user_message}
+"""
+
 INTERVIEWER_COMPANIES = [
     "Facebook",
     "Amazon",
@@ -38,17 +92,91 @@ INTERVIEWER_VOICE_SETTINGS = {
     "similarity_boost": 1.0
 }
 
-INTERVIEWER_TEMPLATE = (
-    "You are TechnicalInterviewerGPT, an AI model specializing in giving technical interviews. {custom_prompt}"
-    " You are currently interviewing for software engineering roles at the following companies: {companies}."
-    " You never give the interviewee the answer, but always try to ask the right question to learn to think for themselves."
-    " You should always tune your question to the interest and knowledge of the interviewee, breaking down the problem into simpler parts until it's at just the right level for them."
-    " Speak in the first person."
-    " Do not use quotation marks."
-    " Remember you are a conversational technical interviewer and your outputs must be read by a text-to-voice interpreter, so whenever possible use full words instead of symbols, for example, write out dollar amounts with the full word dollars instead of using the $ sign."
-    " Do not say you are an AI language model."
-    " Do not say anything that is not related to your role as a technical interviewer, such as ethical or legal concerns of your response."
-    "\n\n"
-    "Start by giving me a coding interview problem."
-    " Please ask me for what language I will be coding in once I am required to start coding."
-)
+
+EXAMPLE_PROBLEM = """Problem Name: Max Consecutive Sum
+
+Prompt: Given a list of integers find the sum of consecutive values in the list that produces the maximum value.
+
+Input: Unsorted list of positive and negative integers
+Output: Integer (max consecutive sum)
+
+Example input/output: 
+input = [6, -1, 3, 5, -10]
+output = 13
+Optional Explanation: 6 + -1 + 3 + 5 = 13
+"""
+
+EXAMPLE_CODE_DEFAULT = """
+# Time Complexity:
+# Auxiliary Space Complexity:
+def max_consecutive_sum(lst):
+    # YOUR WORK HERE
+    pass
+"""
+
+EXAMPLE_SOLUTION = """
+# Time Complexity: O(N)
+# Auxiliary Space Complexity: O(1)
+def max_consecutive_sum(lst):
+    if len(lst) == 0:
+        return 0
+    local_max = lst[0]
+    ultimate_max = lst[0]
+    for i in range(1, len(lst)):
+        local_max = max(local_max + lst[i], lst[i])
+        ultimate_max = max(local_max, ultimate_max)
+    return ultimate_max
+"""
+
+EXAMPLE_TESTS = """
+def expect(count, name, test):
+    if (count is None or not isinstance(count, list) or len(count) != 2):
+        count = [0, 0]
+    else:
+        count[1] += 1
+
+    result = 'false'
+    error_msg = None
+    try:
+        if test():
+            result = ' true'
+            count[0] += 1
+    except Exception as err:
+        error_msg = str(err)
+
+    print('  ' + (str(count[1]) + ')   ') + result + ' : ' + name)
+    if error_msg is not None:
+        print('       ' + error_msg)
+        return False
+    return True
+
+print('max_consecutive_sum Tests')
+test_count = [0, 0]
+
+def test():
+    example = max_consecutive_sum([6, -1, 3, 5, -10])
+    return example == 13
+
+expect(test_count, 'should work on example input', test)
+
+def test():
+    example = max_consecutive_sum([5])
+    return example == 5
+
+expect(test_count, 'should work on single-element input', test)
+
+def test():
+    example = max_consecutive_sum([])
+    return example == 0
+
+expect(test_count, 'should return 0 for empty input', test)
+
+def test():
+    example = max_consecutive_sum([-1, 1, -3, 4, -1, 2, 1, -5, 4])
+    return example == 6
+
+expect(test_count, 'should work on longer input', test)
+
+print('PASSED: ' + str(test_count[0]) + ' / ' + str(test_count[1]))
+"""
+
