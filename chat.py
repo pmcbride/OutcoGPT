1f004a88bd13b21a89f1bcc8a2a6d843074b1353
pmcbride <pmcbride@github.com>
2023-05-18 05:25:47 -0700

    Add Code Box and other components to chat.py

--- chat.py
+++ chat.py
@@ -11,6 +11,11 @@ import subprocess
 from typing import List, Iterator
 import speech_recognition as sr
 import time
+import sys
+
+import whisper
+WHISPER_MODEL = whisper.load_model("base")
+
 
 import config
 from config import (
@@ -42,18 +47,6 @@ messages = [
     }
 ]
 
-audio = generate(
-    text="This is a test.",
-    voice=INTERVIEWER_VOICE,
-    model='eleven_monolingual_v1',
-    stream=False,
-)
-audio_stream = generate(
-    text="This is a test.",
-    voice=INTERVIEWER_VOICE,
-    model='eleven_monolingual_v1',
-    stream=True,
-)
 #%%
 def play_stream(audio):
     # test if audio is iterable
@@ -95,11 +88,26 @@ def stream_to_file(audio_stream: Iterator[bytes], filename: str = None) -> None:
         file.close()
 
 def transcribe(audio, state="", timeout=5):
-    # time.sleep(timeout)
+    time.sleep(timeout)
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     text = transcript["text"]
     state += text + " "
+    return state, state
+
+def transcribe2(audio, state="", timeout=5, model=WHISPER_MODEL):
+    time.sleep(timeout)
+    # create a recognizer
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    options = whisper.DecodingOptions(language= 'en', fp16=False)
+
+    result = whisper.decode(model, mel, options)
+    text = result.text
+    if result.no_speech_prob < 0.5:
+        print(result.text)
+        state += text + " "
     return state, state
 
 def user(user_message, history):
@@ -138,13 +146,13 @@ def chat(history: List[List[str]]):
     response_text = response_message["content"]
     
     # text to speech request with eleven labs
-    audio = generate(
-        text=response_text.replace("\n", " "),
-        voice=INTERVIEWER_VOICE,
-        model='eleven_monolingual_v1',
-        stream=True,
-    )
-    stream(audio)
+    # audio = generate(
+    #     text=response_text.replace("\n", " "),
+    #     voice=INTERVIEWER_VOICE,
+    #     model='eleven_monolingual_v1',
+    #     stream=True,
+    # )
+    # stream(audio)
 
     history[-1][1] = response_text
     return history#, output_filename
@@ -168,50 +176,127 @@ def listen():
         # r.pause_threshold=0.5    
         
         print('Okay, go!')
-        while(1):
-            text = ''
-            print('listening now...')
-            try:
-                audio = r.listen(source, timeout=5, phrase_time_limit=30)
-                print('Recognizing...')
-                # whisper model options are found here: https://github.com/openai/whisper#available-models-and-languages
-                # other speech recognition models are also available.
-                text = r.recognize_whisper(audio, model='medium.en', show_dict=True, )['text']
-            except Exception as e:
-                unrecognized_speech_text = f'Sorry, I didn\'t catch that. Exception was: {e}s'
-                text = unrecognized_speech_text
-            print(text)
+        text = ''
+        print('listening now...')
+        try:
+            audio = r.listen(source, timeout=5, phrase_time_limit=30)
+            print('Recognizing...')
+            # whisper model options are found here: https://github.com/openai/whisper#available-models-and-languages
+            # other speech recognition models are also available.
+            text = r.recognize_whisper(audio, model='medium.en', show_dict=True, )['text']
+        except Exception as e:
+            unrecognized_speech_text = f'Sorry, I didn\'t catch that. Exception was: {e}s'
+            text = unrecognized_speech_text
+        print(text)
     return text
+#%%
+fn_tests = config.EXAMPLE_TESTS
+fn_soln = config.EXAMPLE_SOLUTION
+# print(fn_soln + fn_tests)
+
+def run_tests(fn_str, fn_tests=fn_tests):
+    global_namespace = globals()
+    local_namespace = {}
+    exec(fn_str, global_namespace, local_namespace)
+    global_namespace.update(local_namespace)
+    old_stdout = sys.stdout
+    sys.stdout = captured_stdout = io.StringIO()
+    exec(fn_tests, global_namespace)
+    sys.stdout = old_stdout
+    output = captured_stdout.getvalue()
+    print(output)
+    return output
+
+def run_code(fn_str):
+    """ Run code in Code Box and update global namespace with variables defined in code """
+    global_namespace = globals()
+    local_namespace = {}
+    old_stdout = sys.stdout
+    sys.stdout = captured_stdout = io.StringIO()
+    exec(fn_str, global_namespace, local_namespace)
+    sys.stdout = old_stdout
+    global_namespace.update(local_namespace)
+    output = captured_stdout.getvalue()
+    return output
 
 # set a custom theme
 theme = gr.themes.Default().set(
     body_background_fill="#000000",
 )
+css = """
+#code-block {
+    font-family: 'Consolas', Consolas, monospace;
+    # padding: 10px;
+    # border: 1px solid #ddd;
+    # border-radius: 5px;
+    # background-color: #f5f5f5;
+}
+"""
+# code_default = config.EXAMPLE_SOLUTION
+code_default = config.EXAMPLE_CODE_DEFAULT
 
 with gr.Blocks(theme=theme) as demo:
-    # advisor image input and microphone input
-    advisor = gr.Image(value=config.INTERVIEWER_IMAGE).style(
-        width=config.INTERVIEWER_IMAGE_WIDTH,
-        height=config.INTERVIEWER_IMAGE_HEIGHT
-    )
-    audio_input = gr.Audio(source="microphone", type="filepath")#, streaming=True)
-    audio_text = gr.Textbox(label="Audio Transcript")
-
+    with gr.Row().style(css={'background-color': 'black'}):
+        # interviewer image input and microphone input
+        interviewer = gr.Image(value=config.INTERVIEWER_IMAGE).style(
+            width=config.INTERVIEWER_IMAGE_WIDTH,
+            height=config.INTERVIEWER_IMAGE_HEIGHT,
+            container=True,
+        )
+    with gr.Row():
+        with gr.Column(scale=1):
+            with gr.Box():
+                chatbot = gr.Chatbot([], elem_id="chatbot").style(height=250)
+                audio = gr.Audio(source="microphone", type="filepath", streaming=True)
+                audio_text = gr.Textbox(
+                    show_label=False, 
+                    placeholder="Click the microphone to start recording",
+                    interactive=True
+                ).style(container=False)
+                submit_msg = gr.Button("Submit Message")
+        with gr.Column(scale=1):
+            with gr.Box():
+                code_box = gr.Textbox(
+                    value=code_default,
+                    label="Code Box", 
+                    lines=10,
+                    elem_id="code-block",
+                    interactive=True
+                )
+                run_code_btn = gr.Button("Run Code")
+                submit_code_btn = gr.Button("Submit Code")
+                code_output = gr.Textbox(
+                    value="",
+                    label="Code Output",
+                    lines=1,
+                    elem_id="code-block",
+                    interactive=False
+                )
+    # audio = gr.Audio(source="microphone", type="filepath")#, streaming=True)
+    # audio_text = gr.Textbox(label="Audio Transcript")
+    # record_btn = gr.Button("Record")
+    # record_btn.click(listen, [], [audio_text])
     # text transcript output and audio 
-    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=250)
+    # chatbot = gr.Chatbot([], elem_id="chatbot").style(height=250)
     audio_state = gr.State(value="")
-    audio_input.change(transcribe, [audio_input, audio_state], [audio_text, audio_state]).then(
-        user, [audio_state, chatbot], [audio_state, chatbot]).then(
-        chat, chatbot, chatbot).then(
-            tts, chatbot)
-    # audio_input.stream(transcribe, [audio_input, audio_state], [audio_text, audio_state]).then(
+    # audio.change(transcribe, [audio, audio_state], [audio_text, audio_state]).then(
     #     user, [audio_state, chatbot], [audio_state, chatbot]).then(
     #     chat, chatbot, chatbot).then(
     #         tts, chatbot)
-    btn = gr.Button("Run")
-    btn.click(user, [audio_state, chatbot], [audio_state, chatbot]).then(
-        chat, chatbot, chatbot)#[chatbot, audio_output])
+    audio.stream(transcribe, [audio], [audio_text, audio_state])#.then(
+        # user, [audio_state, chatbot], [audio_state, chatbot]).then(
+        # chat, chatbot, chatbot).then(
+        #     tts, chatbot)
+    audio_text.submit(user, [audio_text, chatbot], [audio_text, chatbot]).then(
+        chat, chatbot, chatbot).then(
+            tts, chatbot)
+    submit_msg.click(user, [audio_text, chatbot], [audio_text, chatbot]).then(
+        chat, chatbot, chatbot).then(
+            tts, chatbot)
+    # btn = gr.Button("Run")
+    # btn.click(user, [audio_state, chatbot], [audio_state, chatbot]).then(
+    #     chat, chatbot, chatbot)#[chatbot, audio_output])
 
 # if __name__ == "__main__":
 demo.queue()
-demo.launch(debug=True, share=False, server_name="0.0.0.0")
+demo.launch(debug=True, share=True, server_name="0.0.0.0")
